86ef77b7d75a8b52dfbd58ff16135a0b844141dc
Nischal Acharya <121549028+Nischal545@users.noreply.github.com>
2025-01-06 23:09:41 +0545

    Add files via upload

---
+++ penaltyJS/script.js
@@ -0,0 +1,9 @@
+// setTimeout(()=>{
+//     const welcome = document.getElementById("welcome");
+//     if(welcome) {
+//         welcome.classList.add("fade-out");
+//         setTimeout(()=>{
+//             welcome.remove();
+//         }, 2000);
+//     }
+// }, 2000)
